4eb3575ea5c674784bad3928e259d913fcca43e4
chemifrandaz <151792763+chemifrandaz@users.noreply.github.com>
2023-12-01 20:49:41 -0300

    Update script.js
    Se amplia

--- script.js
+++ script.js
@@ -21,34 +21,72 @@ console.log("termino el bucle");*/
 palabra ingresa
 recorrer y adivinar letra por letra*/
 
-let palabra = "APPLE"
+//let palabra = "APPLE"
+
+let lista = ("MOUSE", "APPLE", "HOUSE", "GREEN")
+
 let intento = 6;
 
-const button = document.getElementById("guess-button");
+console.log("random"[Math.floor(Math.random())*lista.length])
+let palabra = lista [2]
 
-button.addEventListener('click', intentar);
+const grid = document.getElementById("grid");
+const ROW = document.createElement("div");
+ROW.className = "row;"
+   
 
 function intentar(){
-    const valor = document.getElementById("guess-input");
-    console.log(valor)
-    if (INTENTO === palabra)
+    const VALOR = leerIntento();
+    console.log(VALOR)
+
+
+    if (VALOR === palabra){
+        console.log("ingreso", VALOR);
+
+    }
     
-    console.log("ingreso", valor);
+    
 if (VALOR === palabra){
     console.log("Ganaste");
+    terminar("<h1>GANASTE!😀</h1>")
+    return
 }
 for (i in palabra){
+    const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
     if (palabra[i] === VALOR[i] ){
         console.log(VALOR[i], "verde");
-    }else if(palabra.includes(VALOR[i]))
-    console.log(palabra[i], "amarillo");
-}else (
-    console.log(palabra[i], "gris")
-)
-
+        SPAN.style.backgroundColor = 'green'; 
+        SPAN.innerHTML = VALOR[i];
+    }else if(palabra.includes(VALOR[i])){
+        console.log(palabra[i], "amarillo");
+        SPAN.style.backgroundColor = 'yellow'; 
+        SPAN.innerHTML = VALOR[i];
+    }
+    else{
+        console.log(palabra[i], "gris")
+        SPAN.style.backgroundColor = 'grey'; 
+        SPAN.innerHTML = VALOR[i];
+    }
+    ROW.appendChild (SPAN)
+GRIP.appendChild (ROW)
+}
+intento = intento - 1;
+if (intento ==0){
+    console.log("Perdiste");
+    GamepadButton.style.display = "none";
+}
 }
 
-function leerIntento[]{
-    const VALOR = document.getElementById("quess/input"), value.toUpperCase();
+function leerIntento() {
+    const VALOR = document.getElementById("guess-input").value.toUpperCase();
     return VALOR;
+}
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
 }
